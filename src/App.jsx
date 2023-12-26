7500c2990b225583d8288ba48b9e8cb88b0c151b
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-26 16:45:36 +0530

    Refactor counter functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
+
 function App() {
-  const counter = 1;
+  const [counter, setCounter] = useState(1);
+
+  const increment = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    setCounter((prev) => prev - 1);
+  };
 
   return (
     <>
-      <h1>learn vite react</h1>
+      <h1>learn react</h1>
       <h2>Counter value: {counter}</h2>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
       <p>Footer: {counter}</p>
     </>
   );
