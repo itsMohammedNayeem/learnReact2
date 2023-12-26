23fbd8fc68a5ce996e86a007778459d81fa00132
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-26 16:36:30 +0530

    Add Vite configuration and React components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const counter = 1;
+
+  return (
+    <>
+      <h1>learn vite react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <p>Footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
